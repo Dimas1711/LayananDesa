63d4bd4e6f236258f43fc26a5387005ea6d784c7
dimas1711 <pdimas429@gmail.com>
2020-09-25 19:16:40 +0700

    asd

--- DesaProject/app/src/main/java/com/example/desaproject/MainActivity.java
+++ DesaProject/app/src/main/java/com/example/desaproject/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.desaproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 public class MainActivity extends AppCompatActivity {
-
+    public String TAG;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        throw new RuntimeException("test run");
     }
 }
